a3e08bdef1ba18f6a90a66a289ff7967d95bd144
MstIsratJahanAnika <mstisratjahananika@gmail.com>
2026-02-15 13:41:52 +0600

    use of onclick

---
+++ js/event.js
@@ -0,0 +1,27 @@
+console.log('external file');
+
+// option: function call
+function makeYellow() {
+    document.body.style.backgroundColor = 'yellow';
+}
+
+function makeRed() {
+    document.body.style.backgroundColor = 'red';
+}
+
+// get element by Id and set onclick
+const btnMakeBlue = document.getElementById('btn-make-blue');
+// console.log(btnMakeBlue); 
+btnMakeBlue.onclick = function makeBlue() {
+    document.body.style.backgroundColor = 'blue';
+}
+
+
+// option:  different version
+const makePurple = document.getElementById('btn-make-purple');
+// console.log(makePurple);
+makePurple.onclick = bgPurple;
+
+function bgPurple() {
+    document.body.style.backgroundColor = 'purple';
+}
